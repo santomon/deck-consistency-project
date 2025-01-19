304f88aec10a80812be81faaeb81a59c5aede7c7
santomon <qa12_8@yahoo.de>
2025-01-19 21:51:08 +0100

    ok we have access to the main deck cards now?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,17 @@ import Link from "next/link";
 import * as ydke from "ydke";
 import {ChangeEvent, useState} from "react";
 import {useQueries, useQuery} from "react-query";
-import {YGOCardInfoResponseSchema} from "~/types";
+import {CardInfo, YGOCardInfoResponseSchema} from "~/types";
 
-const createPlaceHolderCardInfoResponse = () => {
-    return { data: []}
+const createPlaceHolderCardInfoResponse = (): CardInfo[] => {
+    return []
 }
 
-const getCardInfo = async (cardIds?: Uint32Array ) => {
+const getCardInfo = async (cardIds?: number[] ) => {
     const commaSeparatedIds = cardIds?.join(',');
+    if (!commaSeparatedIds) {
+        return createPlaceHolderCardInfoResponse();
+    }
     const response = await fetch(`${process.env.NEXT_PUBLIC_CIENTVAR_YGO_CARD_INFO_API_BASE_URL}?id=${commaSeparatedIds}`);
     const responseData = YGOCardInfoResponseSchema.parse(await response.json());
     return responseData.data;
@@ -19,59 +22,97 @@ const getCardInfo = async (cardIds?: Uint32Array ) => {
 export default function Home() {
     const [inputValue, setInputValue] = useState(''); // State maintenance
 
-    const {data: ydkeResult, isSuccess} = useQuery(['ydke', inputValue], async () => {
-        return ydke.parseURL(inputValue);
-    }, {enabled: inputValue.length > 0, initialData: {main: new Uint32Array(), extra: new Uint32Array(), side: new Uint32Array()} });
-    console.log(process.env.NEXT_PUBLIC_CIENTVAR_YGO_CARD_INFO_API_BASE_URL)
+
+    let ydkeResult;
+    try {
+        ydkeResult = ydke.parseURL(inputValue); //
+        ydkeResult = {
+            main: Array.from(ydkeResult.main),
+            extra: Array.from(ydkeResult.extra),
+            side: Array.from(ydkeResult.side),
+        }
+    } catch (e) {
+        console.error(e);
+        ydkeResult = {
+            main: new Array<number>(),
+            extra: new Array<number>(),
+            side: new Array<number>(),
+        };
+    }
+
+    console.log("ydkeResult", ydkeResult)
 
 
     const [mainDeckInfoQueryResult, extraDeckInfoQueryResult, sideDeckInfoQueryResult] = useQueries([
         {
             queryKey: ['cardInfo', ydkeResult?.main],
-            queryFn: () => getCardInfo(ydkeResult?.main),
-            enabled: isSuccess,
+            queryFn: () => getCardInfo(ydkeResult.main),
+            enabled: !!ydkeResult,
         },
         {
             queryKey: ['cardInfo', ydkeResult?.extra],
-            queryFn: () => getCardInfo(ydkeResult?.extra),
-            enabled: isSuccess,
+            queryFn: () => getCardInfo(ydkeResult.extra),
+            enabled: !!ydkeResult,
         },
         {
             queryKey: ['cardInfo', ydkeResult?.side],
-            queryFn: () => getCardInfo(ydkeResult?.side),
-            enabled: isSuccess,
+            queryFn: () => getCardInfo(ydkeResult.side),
+            enabled: !!ydkeResult,
         }
     ])
-    console.log("mainDeckInfo", mainDeckInfoQueryResult);
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setInputValue(e.target.value); // Update state
     };
-  return (
-    <>
-      <Head>
-        <title>Deck Consistency Simulation</title>
-        <meta name="description" content="Deck Consistency Simulation" />
-        {/*<link rel="icon" href="/favicon.ico" />*/}
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+
+    const {data: mainDeckInfo, isError: mainDeckQueryError} = mainDeckInfoQueryResult;
+
+    if (mainDeckQueryError || mainDeckInfo === undefined) {
+        return (
+            <div>
+                <h1>Error fetching main deck info</h1>
+            </div>
+        )
+    }
+
+    const mainDeckCards = ydkeResult.main.map((cardId, index) => {
+        const card = mainDeckInfo.find((card) => card.id === cardId);
+        if (!card) return null;
+        return (
+            <div key={`card-info-display-${cardId}-${index}`}>
+                <h2>{card.name}</h2>
+                <img src={card?.card_images?.[0]?.image_url} alt={card.name} />
+            </div>
+        )
+    }
+    )
+
+    return (
+      <>
+        <Head>
+          <title>Deck Consistency Simulation</title>
+          <meta name="description" content="Deck Consistency Simulation" />
+          {/*<link rel="icon" href="/favicon.ico" />*/}
+        </Head>
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
           <div className="flex flex-col items-center p-4">
-              <label htmlFor="textInput" className="mb-2 text-lg font-medium text-gray-700">
-                  Enter Text
-              </label>
-              <input
-                  id="textInput"
-                  type="text"
-                  value={inputValue}
-                  onChange={handleChange}
-                  placeholder="Type something..."
-                  className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-80"
-              />
-              <p className="mt-4 text-gray-600">
-                  You typed: <span className="font-semibold">{inputValue}</span>
-              </p>
+            <label
+              htmlFor="textInput"
+              className="mb-2 text-lg font-medium text-gray-700"
+            >
+              Enter Text
+            </label>
+            <input
+              id="textInput"
+              type="text"
+              value={inputValue}
+              onChange={handleChange}
+              placeholder="Type something..."
+              className="w-80 rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
           </div>
-      </main>
-    </>
-  );
+            {mainDeckCards}
+        </main>
+      </>
+    );
 }
