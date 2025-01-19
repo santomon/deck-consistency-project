59611076850d74215d53f00dace425ca68af202e
santomon <qa12_8@yahoo.de>
2025-01-19 20:11:39 +0100

    ok initial shit done? now we need to fetch the cards

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import "~/styles/globals.css";
 
+const queryClient = new QueryClient();
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
-      <Component {...pageProps} />
-    </div>
+      <QueryClientProvider client={queryClient}>
+          <div className={GeistSans.className}>
+              <Component {...pageProps} />
+          </div>
+      </QueryClientProvider>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import Head from "next/head";
 import Link from "next/link";
+import * as ydke from "ydke";
+import {ChangeEvent, useState} from "react";
+import {useQuery} from "react-query";
 
 export default function Home() {
+    const [inputValue, setInputValue] = useState(''); // State maintenance
+
+    const {data: ydkeResult, isSuccess} = useQuery(['ydke', inputValue], async () => {
+        return ydke.parseURL(inputValue);
+    }, {enabled: inputValue.length > 0, initialData: {main: new Uint32Array(), extra: new Uint32Array(), side: new Uint32Array()} });
+    console.log(ydkeResult);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value); // Update state
+    };
   return (
     <>
       <Head>
@@ -10,6 +23,22 @@ export default function Home() {
         {/*<link rel="icon" href="/favicon.ico" />*/}
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+          <div className="flex flex-col items-center p-4">
+              <label htmlFor="textInput" className="mb-2 text-lg font-medium text-gray-700">
+                  Enter Text
+              </label>
+              <input
+                  id="textInput"
+                  type="text"
+                  value={inputValue}
+                  onChange={handleChange}
+                  placeholder="Type something..."
+                  className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-80"
+              />
+              <p className="mt-4 text-gray-600">
+                  You typed: <span className="font-semibold">{inputValue}</span>
+              </p>
+          </div>
       </main>
     </>
   );
