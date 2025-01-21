93b7ce235d7b176f08bef75a011dc6aff6acda65
santomon <qa12_8@yahoo.de>
2025-01-21 23:56:32 +0100

    i guess we refactored into 1 query per card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,121 +1,110 @@
 import Head from "next/head";
 import Link from "next/link";
 import * as ydke from "ydke";
-import {ChangeEvent, useState} from "react";
-import {useQueries, useQuery} from "react-query";
-import {CardInfo, YGOCardInfoResponseSchema} from "~/types";
+import { ChangeEvent, useState } from "react";
+import { useQueries, useQuery, useQueryClient } from "react-query";
+import { CardInfo, YGOCardInfoResponseSchema } from "~/types";
 
 const createPlaceHolderCardInfoResponse = (): CardInfo[] => {
-    return []
-}
+  return [];
+};
 
-const getCardInfo = async (cardIds?: number[] ) => {
-    const commaSeparatedIds = cardIds?.join(',');
-    if (!commaSeparatedIds) {
-        return createPlaceHolderCardInfoResponse();
-    }
-    const response = await fetch(`${process.env.NEXT_PUBLIC_CIENTVAR_YGO_CARD_INFO_API_BASE_URL}?id=${commaSeparatedIds}`);
-    const responseData = YGOCardInfoResponseSchema.parse(await response.json());
-    return responseData.data;
-}
+const getCardInfo = async (cardIds?: number[]) => {
+  const commaSeparatedIds = cardIds?.join(",");
+  if (!commaSeparatedIds) {
+    return createPlaceHolderCardInfoResponse();
+  }
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_CIENTVAR_YGO_CARD_INFO_API_BASE_URL}?id=${commaSeparatedIds}`,
+  );
+  const responseData = YGOCardInfoResponseSchema.parse(await response.json());
+  return responseData.data;
+};
+
+const queryKeyFactory = {
+  cardInfo: (cardId: number) => ["cardInfo", cardId],
+};
 
 export default function Home() {
-    const [inputValue, setInputValue] = useState(''); // State maintenance
+  const [inputValue, setInputValue] = useState(""); // State maintenance
+  const queryClient = useQueryClient();
+  const xdd = queryClient.getQueryData<CardInfo[]>(["cardInfo", 1]);
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setInputValue(e.target.value); // Update state
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value); // Update state
+  };
+
+  let ydkeResult;
+  try {
+    ydkeResult = ydke.parseURL(inputValue); //
+    ydkeResult = {
+      main: Array.from(ydkeResult.main),
+      extra: Array.from(ydkeResult.extra),
+      side: Array.from(ydkeResult.side),
     };
+  } catch (e) {
+    ydkeResult = {
+      main: new Array<number>(),
+      extra: new Array<number>(),
+      side: new Array<number>(),
+    };
+  }
 
-    let ydkeResult;
-    try {
-        ydkeResult = ydke.parseURL(inputValue); //
-        ydkeResult = {
-            main: Array.from(ydkeResult.main),
-            extra: Array.from(ydkeResult.extra),
-            side: Array.from(ydkeResult.side),
-        }
-    } catch (e) {
+  console.log("ydkeResult", ydkeResult);
+  const mainDeckIds = ydkeResult.main;
 
-        ydkeResult = {
-            main: new Array<number>(),
-            extra: new Array<number>(),
-            side: new Array<number>(),
-        };
+  const mainDeckQueryResults = useQueries(
+    mainDeckIds.map((cardId) => {
+      return {
+        queryKey: queryKeyFactory.cardInfo(cardId),
+        queryFn: () => getCardInfo([cardId]),
+        initialData: [],
+      };
+    }),
+  );
+  console.log("mainDeckQueryResults", mainDeckQueryResults);
+
+  const mainDeckCards = ydkeResult.main.map((cardId, index) => {
+    if (mainDeckQueryResults?.[index]) {
+      const {isSuccess, isError, data} = mainDeckQueryResults[index];
     }
+    // const card = mainDeckInfo.find((card) => card.id === cardId);
+    // if (!card) return null;
+    // return (
+    //     <div key={`card-info-display-${cardId}-${index}`}>
+    //         <h2>{card.name}</h2>
+    //         <img width={120} src={card.card_images[0]?.image_url} alt={card.name} />
+    //     </div>
+    // )
+    return null;
+  });
 
-    console.log("ydkeResult", ydkeResult)
-    const mainDeckIds = ydkeResult.main;
-
-
-    const [mainDeckInfoQueryResult, extraDeckInfoQueryResult, sideDeckInfoQueryResult] = useQueries([
-        {
-            queryKey: ['cardInfo', ydkeResult.main.join(',')],
-            queryFn: () => getCardInfo(mainDeckIds),
-            enabled: ydkeResult.main.length > 0,
-            initialData: [],
-        },
-        {
-            queryKey: ['cardInfo', ydkeResult.extra.join(',')],
-            queryFn: () => getCardInfo(ydkeResult.extra),
-            enabled: ydkeResult.extra.length >  0,
-            initialData: [],
-        },
-        {
-            queryKey: ['cardInfo', ydkeResult.side.join(',')],
-            queryFn: () => getCardInfo(ydkeResult.side),
-            enabled: ydkeResult.side.length > 0,
-            initialData: [],
-        }
-    ])
-
-    const {data: mainDeckInfo, isError: mainDeckQueryError} = mainDeckInfoQueryResult;
-
-    if (mainDeckQueryError || mainDeckInfo === undefined) {
-        return (
-            <div>
-                <h1>Error fetching data from ygoprodecks server</h1>
-            </div>
-        )
-    }
-
-    const mainDeckCards = ydkeResult.main.map((cardId, index) => {
-        const card = mainDeckInfo.find((card) => card.id === cardId);
-        if (!card) return null;
-        return (
-            <div key={`card-info-display-${cardId}-${index}`}>
-                <h2>{card.name}</h2>
-                <img width={120} src={card.card_images[0]?.image_url} alt={card.name} />
-            </div>
-        )
-    }
-    )
-
-    return (
-      <>
-        <Head>
-          <title>Deck Consistency Simulation</title>
-          <meta name="description" content="Deck Consistency Simulation" />
-          {/*<link rel="icon" href="/favicon.ico" />*/}
-        </Head>
-        <main className="flex min-h-screen flex-col items-center justify-center bg-teal-700">
-          <div className="flex flex-col items-center p-4">
-            <label
-              htmlFor="textInput"
-              className="mb-2 text-lg font-medium text-gray-700"
-            >
-              Enter Text
-            </label>
-            <input
-              id="textInput"
-              type="text"
-              value={inputValue}
-              onChange={handleChange}
-              placeholder="Type something..."
-              className="w-80 rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-            {mainDeckCards}
-        </main>
-      </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Deck Consistency Simulation</title>
+        <meta name="description" content="Deck Consistency Simulation" />
+        {/*<link rel="icon" href="/favicon.ico" />*/}
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-teal-700">
+        <div className="flex flex-col items-center p-4">
+          <label
+            htmlFor="textInput"
+            className="mb-2 text-lg font-medium text-gray-700"
+          >
+            Enter Text
+          </label>
+          <input
+            id="textInput"
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Type something..."
+            className="w-80 rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        {mainDeckCards}
+      </main>
+    </>
+  );
 }
