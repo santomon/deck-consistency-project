8d2254e8d707fa52e0daa025bebeefaf4f5385ef
santomon <qa12_8@yahoo.de>
2025-01-23 00:23:44 +0100

    adapting with a store

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -107,6 +107,12 @@ export default function Home() {
             placeholder="Type something..."
             className="w-80 rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+          <button
+            onClick={handleYDKEButtonSubmitted}
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm"
+            >
+            Parse YDKE
+          </button>
         </div>
         {mainDeckCards}
       </main>
